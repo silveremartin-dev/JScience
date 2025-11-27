197c33d8581d375c1ca0d225899e634e0cddb59d
Silvere Martin-Michiellot <silvere.martin@gmail.com>
2025-11-27 19:54:51 +0100

    WIP: Start weighted graphs implementation - interfaces
    Created core weighted graph interfaces:
    - WeightedEdge<V,W> - edge with source, target, weight
    - DefaultWeightedEdge<V,W> - immutable implementation
    
    Next: WeightedGraph interface, DirectedWeightedGraph impl, Dijkstra
    
    Part of weighted graphs feature (Phase 3)

---
+++ src/main/java/org/jscience/mathematics/discrete/DefaultWeightedEdge.java
@@ -0,0 +1,92 @@
+/*
+ * JScience - Java(TM) Tools and Libraries for the Advancement of Sciences.
+ * Copyright (C) 2025 - Silvere Martin-Michiellot (silvere.martin@gmail.com)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.jscience.mathematics.discrete;
+
+import java.util.Objects;
+
+/**
+ * Default implementation of a weighted edge.
+ * 
+ * @param <V> the type of vertices
+ * @param <W> the type of weights
+ * 
+ * @author Silvere Martin-Michiellot
+ * @author Gemini AI (Google DeepMind)
+ * @since 1.0
+ */
+public final class DefaultWeightedEdge<V, W> implements WeightedEdge<V, W> {
+
+    private final V source;
+    private final V target;
+    private final W weight;
+
+    /**
+     * Creates a new weighted edge.
+     * 
+     * @param source the source vertex
+     * @param target the target vertex
+     * @param weight the edge weight
+     */
+    public DefaultWeightedEdge(V source, V target, W weight) {
+        this.source = Objects.requireNonNull(source, "Source vertex cannot be null");
+        this.target = Objects.requireNonNull(target, "Target vertex cannot be null");
+        this.weight = Objects.requireNonNull(weight, "Weight cannot be null");
+    }
+
+    @Override
+    public V getSource() {
+        return source;
+    }
+
+    @Override
+    public V getTarget() {
+        return target;
+    }
+
+    @Override
+    public W getWeight() {
+        return weight;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (!(obj instanceof WeightedEdge))
+            return false;
+        WeightedEdge<?, ?> other = (WeightedEdge<?, ?>) obj;
+        return source.equals(other.getSource()) &&
+                target.equals(other.getTarget()) &&
+                weight.equals(other.getWeight());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(source, target, weight);
+    }
+
+    @Override
+    public String toString() {
+        return source + " --(" + weight + ")--> " + target;
+    }
+}
---
+++ src/main/java/org/jscience/mathematics/discrete/WeightedEdge.java
@@ -0,0 +1,87 @@
+/*
+ * JScience - Java(TM) Tools and Libraries for the Advancement of Sciences.
+ * Copyright (C) 2025 - Silvere Martin-Michiellot (silvere.martin@gmail.com)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.jscience.mathematics.discrete;
+
+/**
+ * An edge in a weighted graph.
+ * <p>
+ * Represents a directed edge from a source vertex to a target vertex
+ * with an associated weight. Can also represent undirected edges.
+ * </p>
+ *
+ * @param <V> the type of vertices
+ * @param <W> the type of weights
+ * 
+ * @author Silvere Martin-Michiellot
+ * @author Gemini AI (Google DeepMind)
+ * @since 1.0
+ */
+public interface WeightedEdge<V, W> {
+
+    /**
+     * Returns the source vertex of this edge.
+     * 
+     * @return the source vertex
+     */
+    V getSource();
+
+    /**
+     * Returns the target vertex of this edge.
+     * 
+     * @return the target vertex
+     */
+    V getTarget();
+
+    /**
+     * Returns the weight of this edge.
+     * 
+     * @return the edge weight
+     */
+    W getWeight();
+
+    /**
+     * Checks if this edge connects the given vertices.
+     * <p>
+     * For undirected interpretation, returns true if the edge connects
+     * u to v OR v to u.
+     * </p>
+     * 
+     * @param u the first vertex
+     * @param v the second vertex
+     * @return true if this edge connects u and v (in either direction)
+     */
+    default boolean connects(V u, V v) {
+        return (getSource().equals(u) && getTarget().equals(v)) ||
+                (getSource().equals(v) && getTarget().equals(u));
+    }
+
+    /**
+     * Checks if this edge is incident to the given vertex.
+     * 
+     * @param vertex the vertex to check
+     * @return true if vertex is source or target
+     */
+    default boolean isIncidentTo(V vertex) {
+        return getSource().equals(vertex) || getTarget().equals(vertex);
+    }
+}
