1d08722fadb2668f72478f4fe1504112e4742d89
Silvere Martin-Michiellot <silvere.martin@gmail.com>
2025-12-23 02:19:05 +0100

    Fix launchers: Localize dependencies and add Main Demo Suite launcher
    - Refactor: Updated run-civilization.bat and run-crispr.bat to use local 'lib' folder
    - Feature: Added run-jscience-demos.bat for launching the main demo suite
    - Feature: Added setup-launchers.bat to automate dependency copying
    - Fix: Resolved 'Module javafx.controls not found' error by ensuring correct module path

--- launchers/run-jscience-demos.bat
+++ launchers/run-jscience-demos.bat
@@ -1,11 +1,11 @@
 @echo off
 setlocal
 
-set APP_CLASS=org.jscience.apps.social.CivilizationApp
+set APP_CLASS=org.jscience.ui.JScienceDemoApp
 set LIB_DIR=lib
 set MODULE_PATH=..\jscience-killer-apps\target\classes;..\jscience-core\target\classes;..\jscience-natural\target\classes;..\jscience-social\target\classes;..\jscience-mathematics\target\classes
 
-echo Starting Civilization Collapse Model...
+echo Starting JScience Demo Suite...
 java --module-path "%LIB_DIR%" --add-modules javafx.controls,javafx.graphics,javafx.fxml -cp "%MODULE_PATH%;%LIB_DIR%\*" %APP_CLASS%
 
 endlocal
---
+++ launchers/setup-launchers.bat
@@ -0,0 +1,11 @@
+@echo off
+echo Setting up launcher dependencies...
+echo This script will download required JARs to the 'lib' directory.
+
+cd ..\jscience-killer-apps
+call mvn dependency:copy-dependencies "-DoutputDirectory=../launchers/lib" "-DincludeScope=runtime"
+cd ..\launchers
+
+echo.
+echo Setup complete. You can now run the batch files.
+pause
